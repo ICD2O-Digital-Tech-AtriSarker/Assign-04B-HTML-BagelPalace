65c7e2debed1bb1e1f204dfb3eca99076d890c81
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-05-08 00:13:35 +0000

    Sandwich display

--- js/input.js
+++ js/input.js
@@ -34,6 +34,7 @@ function addTopping(toppingName, pos = "default") {
   dropdownList.value = toppingName;
   dropdownList.id = `topping${pos}`
 
+  dropdownList.addEventListener('change', drawSandwich);
 }
 
 amountOfToppingsInput.oninput = () => {
@@ -61,7 +62,7 @@ amountOfToppingsInput.oninput = () => {
 
   if (currentAmount < amount) {
     for (let i=currentAmount; i < amount; i++) {
-      addTopping('egg');
+      addTopping('sausage-patty');
     }
   }
   else {
@@ -69,10 +70,98 @@ amountOfToppingsInput.oninput = () => {
       toppingsContainer.lastChild.remove();
     }
   }
-  
+
+  drawSandwich();
 }
 
 
 addTopping('sausage-patty');
 addTopping('cheese');
 addTopping('egg');
+
+
+// DISPLAY SANDWICH CODE
+
+// Get the canvas element for displaying the sandwich
+const sandwichDisplay = document.getElementById('sandwichDisplay');
+
+// Get the render/draw element for the canvas
+const render = sandwichDisplay.getContext('2d');
+
+// FILEPATH FOR TOPPING IMAGES
+const imagePath = './.././images/toppings/';
+
+// function that gets image
+function image(toppingName) {
+  let img = new Image();
+  img.src = imagePath + toppingName + '.png';
+  return img
+}
+
+let version = 0;
+// function that draws the sandwich()
+function drawSandwich() {
+
+  version += 1;
+  let currentVersion = version;
+
+  // Clear canvas
+  render.clearRect(0, 0, sandwichDisplay.width, sandwichDisplay.height);
+
+  
+  let toppingList = []
+
+  toppingList.push("top-bagel")
+  // Get all dropdowns using queryselector
+  let toppingDropdowns = document.querySelectorAll('select');
+  for (let i = 0; i < toppingDropdowns.length; i++) {
+    let topping = toppingDropdowns[i].value;
+    toppingList.push(topping);
+  }
+  toppingList.push("bottom-bagel")
+
+  // reverse the list, as drawing will be done bottom to top
+  toppingList.reverse();
+  console.log(toppingList)
+
+  
+
+  // Resize canvas to fit sandwich (27px per topping)
+  sandwichDisplay.height = 27 * (toppingList.length+1)
+  
+  // Get canvas width and height
+  let canvasWidth = sandwichDisplay.width;
+  let canvasHeight = sandwichDisplay.height;
+
+  // Dimensions for the topping Images
+  let toppingHeight = (canvasHeight) / (1+ toppingList.length*0.25);
+  let toppingWidth = Math.min(canvasWidth,toppingHeight * 1.8);
+  let drawX = canvasWidth / 2 - toppingWidth / 2;
+
+  // Recursion function to draw the toppings
+  let drawTopping = (zIndex) => {
+    if (zIndex >= toppingList.length ) {
+      return
+    }
+    if (currentVersion != version) {
+      return
+    }
+    let toppingName = toppingList[zIndex];
+    let img = image(toppingName);
+    img.onload = () => {
+      let drawY = canvasHeight - toppingHeight * (zIndex/4 + 1)
+      render.drawImage(img, drawX, drawY, toppingWidth, toppingHeight);
+      drawTopping(zIndex + 1);
+    }
+  }
+
+  drawTopping(0);
+  
+
+  console.log(1)
+  
+}
+
+drawSandwich();
+
+// Connect dropdown selection change to drawSandwich
