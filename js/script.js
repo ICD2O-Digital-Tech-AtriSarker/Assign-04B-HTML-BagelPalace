228ef66f1d3440e9fa73445683449362e8bde779
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-05-09 02:08:56 +0000

    Very substantial commit [ LOTS OF COMMENTS ]

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
+/*
+* Created by: Atri Sarker
+* Created on: May, 2024
+* Description: This file contains the script.js for the Burger Palace website. This script is in charge of getting the user input/order and calculating+displaying the subtotal, tax, and total of the order.
+*/
+'use strict'; // Strict mode
 
 // Elements
 
-// FORM
+// INPUT FORM
 const inputForm = document.getElementById("inputForm");
-// Get all radio buttons with the name "size"
+
+// List of the radio buttons for the size selection
 var radios = document.getElementsByName('size');
-// Toppings Div
-const toppingsContainer = document.getElementById('toppingsContainer');
+
+//  Container for Topping Dropdowns, not used
+// const toppingsContainer = document.getElementById('toppingsContainer');
+
 // Side orders and drinks
 const onionRingsInput = document.getElementById('onionRingsAmount');
 const frenchFriesInput = document.getElementById('friesAmount');
 const waterInput = document.getElementById('waterAmount');
 const pepsiInput = document.getElementById('pepsiAmount');
+
 // Result Button
 const resultBtn = document.getElementById('resultBtn');
-// Output Result
+// Output Result Container
 const resultDiv = document.getElementById('resultDiv');
 
 // COSTS
-// Size Cost
+// Size Costs
 const sizeCosts = {
   "small": 3.00,
   "medium": 5.00,
@@ -52,13 +62,14 @@ const toppingCosts = {
   "ranch": 0.10
 }
 
-// Size Names
+// Size Names, for reciept charges
 const sizeNames = {
   "small": "Small Size",
   "medium": "Medium Size",
   "large": "Large Size"
 }
-// Toppings Names
+
+// Toppings Names, for reciept charges
 const toppingNames = {
   "sausage-patty": "Sausage Patty 🍖",
   "vegetable-patty": "Vegetable Patty 🥦",
@@ -86,23 +97,29 @@ const pepsiCost = 4.00;
 // TAX
 const HST = 0.13;
 
+// main function, called upon button click [form submission]
 function getResult() {
+
+  // Get size
   let size = null;
   // Loop through each radio button
   for (let i = 0; i < radios.length; i++) {
     // Check if the radio button is checked
     if (radios[i].checked) {
-      // If checked, store its value
+      // If checked, set size to the value
       size = radios[i].value;
-      // Break the loop as we've found the selected radio button
+      // Break the loop because the selected radio button has been found
       break;
     }
   }
 
-  // List of toppings, currently empty
+  // List of toppings, initialized as empty
   let toppings = {};
-  // Get all dropdowns using queryselector
+  
+  // Get all topping dropdowns using queryselector
   let toppingDropdowns = document.querySelectorAll('select');
+  
+  // For each dropdown
   for (let i = 0; i < toppingDropdowns.length; i++) {
     let topping = toppingDropdowns[i].value;
 
@@ -116,6 +133,7 @@ function getResult() {
     }
   }
   // Toppings are now contained within toppings{}
+  // toppings{key (topping name): value (number of toppings)}
 
   // Side orders and drinks
   let onionRingsAmount = parseInt(onionRingsInput.value);
@@ -124,49 +142,65 @@ function getResult() {
   let pepsiAmount = parseInt(pepsiInput.value);
 
 
-
+  // Initialize receipt, list that contains/logs all charges
   let reciept = [];
-  // Add size cost to reciept
+  
+  // Get size cost
   let sizeCost = sizeCosts[size];
+  // Add size cost to reciept
   reciept.push(`+$${sizeCost.toFixed(2)} [${sizeNames[size]}]`)
 
-  // Add toppings cost to reciept
+  // Get toppings cost
   let summedToppingsCost = 0.00;
+  // Uses a for in loop to sum the cost of each topping
   for (let i = 0; i < Object.keys(toppings).length; i++) {
-    console.log(i);
+    // Get topping Name
     let topping = Object.keys(toppings)[i];
-    console.log(topping);
+    // Get cost of topping
     let toppingCost = toppingCosts[topping];
+    // Get amount of topping
     let toppingAmount = toppings[topping];
+    // multiply cost with amount to get total cost for topping
     let toppingCostTotal = toppingCost * toppingAmount;
+    // Add it to the total topping cost sum
     summedToppingsCost += toppingCostTotal;
-    reciept.push(`+$${toppingCostTotal.toFixed(2)} [${toppingNames[topping]} x${toppingAmount}]`)
+
+    // Add charge to reciept
+    reciept.push(`+$${toppingCostTotal.toFixed(2)} [${toppingNames[topping]} x${toppingAmount}]`);
   }
 
-  // Add Topping Premium to reciept
+  // Get Topping Premium [30% extra cost for large]]
   let toppingPremium = summedToppingsCost * toppingsValue[size];
-  reciept.push(`+$${toppingPremium.toFixed(2)} [${sizeNames[size]} toppings Increment]`)
+  // Add the charge to the reciept
+  reciept.push(`+$${toppingPremium.toFixed(2)} [${sizeNames[size]} Toppings additional cost]`);
 
-  // Add side orders and drinks cost to reciept
+  // Initialize variable to store summed cost for side-orders and drinks
   let summedSideOrderAndDrinksCost = 0.00;
+
+  // Go through each side-order and drink
+  // If ordered, add the total cost (amount*cost) to the summed cost
+  // also, add the charge to the reciept
+
+  // Onion Rings
   if (onionRingsAmount>0) {
     let cost = onionRingsAmount * onionRingsCost;
     summedSideOrderAndDrinksCost += cost;
     reciept.push(`+$${cost.toFixed(2)} [Onion Rings x${onionRingsAmount}]`);
   }
+  // French Fries
   if (frenchFriesAmount>0) {
     let cost = frenchFriesAmount * frenchFriesCost;
     summedSideOrderAndDrinksCost += cost;
     reciept.push(`+$${cost.toFixed(2)} [French Fries x${frenchFriesAmount}]`);
   }
-  // for pepsi
+  // Pepsi
   if (pepsiAmount>0) {
     let cost = pepsiAmount * pepsiCost;
     summedSideOrderAndDrinksCost += cost;
     reciept.push(
       `+$${cost.toFixed(2)} [500ml Pepsi x${pepsiAmount}]`);
   }
-  // for water 
+  // Water
   if (waterAmout>0) {
     let cost = waterAmout * waterCost;
     summedSideOrderAndDrinksCost += cost;
@@ -179,16 +213,17 @@ function getResult() {
   let tax = subtotal * HST;
   let total = subtotal + tax;
 
-  // Add subtotal, tax, and total to reciept
+  // Add subtotal, tax, and total to reciept [bolded]
   reciept.push(`<b>Subtotal: $${subtotal.toFixed(2)}</b>`);
   reciept.push(`<b>HST: $${tax.toFixed(2)}</b>`);
   reciept.push(`<b>Total: $${total.toFixed(2)}</b>`);
 
-  // Display
+  // Display the reciept, all charges separed by a line break
   resultDiv.innerHTML = reciept.join("<br>");
 }
 
-
+// Connect form submission to getResult()
+// returns false to prevent page from reloading
 inputForm.onsubmit = () => {
   getResult();
   return false;
