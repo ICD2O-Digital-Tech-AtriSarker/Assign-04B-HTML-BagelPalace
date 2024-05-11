82973eb1252fd0b10d7a1a4d25c4291faef8f632
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-05-11 02:08:45 +0000

    fix

--- js/input.js
+++ js/input.js
@@ -27,10 +27,10 @@ function getChild(parent, elementType) {
   let children = parent.childNodes;
   // Loop through children
   for (let i = 0; i < children.length; i++) {
-    // If child matches the elementType, return it
-    if (children[i].nodeName.toLowerCase() == elementType) {
-      return children[i];
-    }
+  // If child matches the elementType, return it
+  if (children[i].nodeName.toLowerCase() == elementType) {
+    return children[i];
+  }
   }
   // If no child matched, returns null
   return null;
@@ -48,7 +48,7 @@ function addTopping(toppingName, pos = "default") {
   // Note: Originally it was made like this so I could do drag and drop functionality by setting the positions through the function
   // I never used a pos other than "default" since I scrapped the drag and drop feature
   if (pos == "default") {
-    pos = toppingsContainer.childNodes.length;
+  pos = toppingsContainer.childNodes.length;
   }
 
   // New Topping Dropdown, Clone of the template
@@ -68,7 +68,7 @@ function addTopping(toppingName, pos = "default") {
   dropdownList.id = `topping${pos}`
   dropdownList.name = `topping${pos}`
 
-  // Connect dropwdown value change to the drawSandwich function 
+  // Connect dropdown value change to the drawSandwich function
   // located at [bottom of the script file]
   dropdownList.addEventListener('change', drawSandwich);
 }
@@ -79,55 +79,55 @@ amountOfToppingsInput.oninput = () => {
   let amount = Number(amountOfToppingsInput.value);
   // Get max amount
   let maxAmount = Number(amountOfToppingsInput.getAttribute("max"));
-  
+
   if (amount > maxAmount) {
-    // If the amount is greater than the max amount, set the amount to the max amount
-    // Prevents the user from entering more toppings than the max amount
-    amount = maxAmount;
-    amountOfToppingsInput.value = amount;
+  // If the amount is greater than the max amount, set the amount to the max amount
+  // Prevents the user from entering more toppings than the max amount
+  amount = maxAmount;
+  amountOfToppingsInput.value = amount;
   }
   else if (amount < 0) {
-    // Prevents negative input by always keeping it at a non-negative value
-    amount = 0;
-    amountOfToppingsInput.value = amount;
+  // Prevents negative input by always keeping it at a non-negative value
+  amount = 0;
+  amountOfToppingsInput.value = amount;
   }
   else if ( amount % 1 != 0 ) {
-    // Prevents non-integer input by always keeping it at an integer value
-    amount = Math.floor(amount);
-    amountOfToppingsInput.value = amount;
+  // Prevents non-integer input by always keeping it at an integer value
+  amount = Math.floor(amount);
+  amountOfToppingsInput.value = amount;
   }
 
   // Current list of children of the toppings container
   let current = toppingsContainer.childNodes;
-  
+
   // Current amount of topping dropdowns
   let currentAmount = current.length;
 
   if (currentAmount == amount) {
-    // If input is equal to the current amount, do nothing
-    // amount of dropdowns will stay the same
-    return;
+  // If input is equal to the current amount, do nothing
+  // amount of dropdowns will stay the same
+  return;
   }
 
   if (currentAmount < amount) {
-    // If input is greater than the current amount
-    // Then : add more dropdowns till the amount reaches the input
-    for (let i=currentAmount; i < amount; i++) {
-      // Adds a dropdown, defaulted value of sausage-patty
-      addTopping('sausage-patty');
-    }
+  // If input is greater than the current amount
+  // Then : add more dropdowns till the amount reaches the input
+  for (let i=currentAmount; i < amount; i++) {
+    // Adds a dropdown, defaulted value of sausage-patty
+    addTopping('sausage-patty');
+  }
   }
   else {
-    // If input is less than the current amount
-    // Then : remove dropdowns until the amount reaches the input
-    for (let i=currentAmount; i > amount; i--) {
-      // Deletes the bottom-most dropdown
-      toppingsContainer.lastChild.remove();
-    }
+  // If input is less than the current amount
+  // Then : remove dropdowns until the amount reaches the input
+  for (let i=currentAmount; i > amount; i--) {
+    // Deletes the bottom-most dropdown
+    toppingsContainer.lastChild.remove();
+  }
   }
 
   // Draw the sandwich, this function is located at the bottom of the script file
-  // This is done to display the sandwich with the newly added toppings from the dropwdowns.
+  // This is done to display the sandwich with the newly added toppings from the dropdowns.
   drawSandwich();
 }
 
@@ -140,7 +140,7 @@ const sandwichDisplay = document.getElementById('sandwichDisplay');
 // Get the render/draw element for the canvas
 const render = sandwichDisplay.getContext('2d');
 
-// FILEPATH FOR TOPPING IMAGES
+// FILE PATH FOR TOPPING IMAGES
 const imagePath = './images/toppings/';
 
 // function that gets image using filepath and name/value
@@ -168,36 +168,36 @@ function drawSandwich() {
 
   // Add "top-bagel" to list
   toppingList.push("top-bagel")
-  
+
   // Get all dropdowns using queryselector order[top-bottom]
   let toppingDropdowns = document.querySelectorAll('select');
   // Loop through all selections and add them to the list
   for (let i = 0; i < toppingDropdowns.length; i++) {
-    let topping = toppingDropdowns[i].value;
-    toppingList.push(topping);
+  let topping = toppingDropdowns[i].value;
+  toppingList.push(topping);
   }
   // Add "bottom-bagel" to list
   toppingList.push("bottom-bagel")
 
-  // reverse the list, as drawing will be done from bottom to top, 
+  // reverse the list, as drawing will be done from bottom to top,
   // starting from "bottom-bagel"
   toppingList.reverse();
 
   // Resize canvas to fit sandwich (27px per topping)
   sandwichDisplay.height = 27 * (toppingList.length+1)
-  
+
   // Get canvas width and height
   let canvasWidth = sandwichDisplay.width;
   let canvasHeight = sandwichDisplay.height;
 
   // Calculate the Dimensions for the topping Images
-  
+
   // Height
   let toppingHeight = (canvasHeight) / (1+ toppingList.length*0.25);
   // Width, Math.min to prevent width from surpassing canvas width
   let toppingWidth = Math.min(canvasWidth,toppingHeight * 1.8);
 
-  // Images are drawn from their top-left postion
+  // Images are drawn from their top-left position
   // drawX ensures the image is drawn in the middle of the canvas x-axis
   let drawX = canvasWidth / 2 - toppingWidth / 2;
 
@@ -206,30 +206,30 @@ function drawSandwich() {
   // A topping would only be drawn when the previous topping finishes loading.
   // vice-versa for future toppings, until all toppings are drawn
   let drawTopping = (zIndex) => {
-    
-    if (zIndex >= toppingList.length ) {
-      // Escape condtion, returns once all toppings are drawn
-      return
-    }
-    if (currentVersion != version) {
-      // Stops the drawing if a new drawing/display has started
-      return
-    }
 
-    // Get the topping name/value
-    let toppingName = toppingList[zIndex];
-    // Get the topping's image using the topping name/value
-    let img = image(toppingName);
+  if (zIndex >= toppingList.length ) {
+    // Escape condition, returns once all toppings are drawn
+    return
+  }
+  if (currentVersion != version) {
+    // Stops the drawing if a new drawing/display has started
+    return
+  }
 
-    // Connect onload to draw function, to ensure the image is loaded before drawing
-    img.onload = () => {
-      // get drawY
-      let drawY = canvasHeight - toppingHeight * (zIndex/4 + 1)
-      // Draws the image
-      render.drawImage(img, drawX, drawY, toppingWidth, toppingHeight);
-      // Draw the next topping on top of this topping, recursion
-      drawTopping(zIndex + 1);
-    }
+  // Get the topping name/value
+  let toppingName = toppingList[zIndex];
+  // Get the topping's image using the topping name/value
+  let img = image(toppingName);
+
+  // Connect onload to draw function, to ensure the image is loaded before drawing
+  img.onload = () => {
+    // get drawY
+    let drawY = canvasHeight - toppingHeight * (zIndex/4 + 1)
+    // Draws the image
+    render.drawImage(img, drawX, drawY, toppingWidth, toppingHeight);
+    // Draw the next topping on top of this topping, recursion
+    drawTopping(zIndex + 1);
+  }
   }
 
   // Draw the first topping, will always be "bottom-bagel"
@@ -248,4 +248,5 @@ amountOfToppingsInput.value = 3;
 
 // Initial Draw for the default sandwich
 drawSandwich();
+
 
